03072535e417a0a9bc6fc5e8b74533d8ba380d11
Alperenhub <kilincfurkanalperen@gmail.com>
2025-11-25 15:32:29 +0300

    message controller fixed

--- api/src/lib/socket.js
+++ api/src/lib/socket.js
@@ -1,42 +1,46 @@
-import {Server} from "socket.io";
+// lib/socket.js
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import {ENV} from "./env.js";
+import { ENV } from "./env.js";
 import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
 
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server( server, {
-    cors: {
-        origin: [ENV.CLIENT_URL],
-        credentials: true,
-    },
+// Socket.io server
+const io = new Server(server, {
+  cors: {
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  },
 });
 
 io.use(socketAuthMiddleware);
 
-export function getReceiverSocketId(userId){
-    return userSocketMap[userId];
+// Kullanıcı socket map
+const userSocketMap = {}; // {userId: socketId}
+
+// Kullanıcı ID'den socket ID almak için
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
+// Socket.io bağlantısı
+io.on("connection", (socket) => {
+  console.log("Kullanıcı bağlandı:", socket.user.fullName);
 
-const userSocketMap = {}; // {userId:socketId}
+  const userId = socket.user._id.toString();
+  userSocketMap[userId] = socket.id;
 
-io.on("connection", (socket)=> {
-    console.log("Kulllanıcı bağlandı", socket.user.fullName);
+  // Online kullanıcıları gönder
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    const userId = socket.userId
-    userSocketMap[userId] = socket.id
-
+  socket.on("disconnect", () => {
+    console.log("Kullanıcı bağlantısı kesildi:", socket.user.fullName);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", ()=> {
-        console.log("Kullanıcı bağlantısı kesildi:", socket.user.fullName);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+  });
 });
 
-
-export {io, app, server};
+export { io, app, server };
